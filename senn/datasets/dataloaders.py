14a7b798433a860e9a447b4fa90390101f1729cd
ldeis <deislukas@gmail.com>
2020-10-21 17:07:01 +0200

    purged MINST set related code (not needed)

--- senn/datasets/dataloaders.py
+++ senn/datasets/dataloaders.py
@@ -24,60 +24,12 @@ def get_dataloader(config):
     -------
     Corresponding dataloader.
     """
-    if config.dataloader.lower() == 'mnist':
-        return load_mnist(**config.__dict__)
+    if config.dataloader.lower() == 'umc':
+        return load_umc(**config.__dict__)
     elif config.dataloader.lower() == 'compas':
         return load_compas(**config.__dict__)
-
-
-def load_mnist(data_path, batch_size, num_workers=0, valid_size=0.1, **kwargs):
-    """
-    Load mnist data.
-
-    Loads mnist dataset and performs the following preprocessing operations:
-        - converting to tensor
-        - standard mnist normalization so that values are in (0, 1)
-
-    Parameters
-    ----------
-    data_path: str
-        Location of mnist data.
-    batch_size: int
-        Batch size.
-    num_workers: int
-        the number of  workers to be used by the Pytorch DataLoaders
-    valid_size : float
-        a float between 0.0 and 1.0 for the percent of samples to be used for validation
-
-    Returns
-    -------
-    train_loader
-        Dataloader for training set.
-    valid_loader
-        Dataloader for validation set.
-    test_loader
-        Dataloader for testing set.
-    """
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
-
-    train_set = datasets.MNIST(data_path, train=True, download=True, transform=transform)
-    test_set = datasets.MNIST(data_path, train=False, download=True, transform=transform)
-
-    train_size = len(train_set)
-    split = int(np.floor(valid_size * train_size))
-    indices = list(range(train_size))
-    train_sampler = SubsetRandomSampler(indices[split:])
-    valid_sampler = SubsetRandomSampler(indices[:split])
-
-    dataloader_args = dict(batch_size=batch_size, num_workers=num_workers, drop_last=True)
-    train_loader = DataLoader(train_set, sampler=train_sampler, **dataloader_args)
-    valid_loader = DataLoader(train_set, sampler=valid_sampler, **dataloader_args)
-    test_loader = DataLoader(test_set, shuffle=False, **dataloader_args)
-
-    return train_loader, valid_loader, test_loader
+    else:
+        RuntimeError("The specified dataset has not been found")
 
 
 #  --------------- Compas Dataset  ---------------
